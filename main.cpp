100121b839238a00004cc301e8f4596d32abbea2
Geissel Espino <98473712+Gleizits@users.noreply.github.com>
2022-05-05 22:00:35 +0000

    Thhis is a replit test.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+  float x;
+  cout<<"Digits a number"<<endl; cin>>x;
+  cout<<"Your number is:"<<x<<endl;
+  getch();
+  return 0;
+}
